409c1994c6210db0720094af8a4bb650691d96a0
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2023-08-08 20:20:12 -0400

    Add files via upload

---
+++ main/java/com/example/jieunworkouttracker/CalendarItem.java
@@ -0,0 +1,34 @@
+package com.example.jieunworkouttracker;
+
+
+
+public class CalendarItem {
+    private String title;
+    private String workout_id;
+    private String date;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setWorkoutId(String workout_id) {
+        this.workout_id = workout_id;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWorkoutId() {
+        return workout_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+}
